dacfe218d329577af01e438cda3746ee758f187d
bitko <marcelina.bee@gmail.com>
2022-02-19 17:18:06 +0100

    adding floor

--- index.php
+++ index.php
@@ -17,6 +17,10 @@
 <canvas id="canvas" width="800" height="600"></canvas>
 <img id="cloud-image" src="img/cloud1.png">
 <img id="player-image" src="img/p3_front.png">
+<img id="snowLeft-image" src="img/snowLeft.png">
+<img id="snowMid-image" src="img/snowMid.png">
+<img id="snowRight-image" src="img/snowRight.png">
+<img id="stoneCenter-image" src="img/stoneCenter.png">
 <script>
     function getRandomInt(min, max) {
         min = Math.ceil(min);
@@ -47,6 +51,15 @@
                 this.x -= 7
         }
     }
+
+    class Snow {
+        constructor(x, y, size) {
+            this.x = x;
+            this.y = y;
+            this.size = size
+        }
+    }
+
     window.setTimeout(function () {
         let canvas = document.getElementById('canvas')
         let ctx = canvas.getContext('2d');
@@ -54,7 +67,11 @@
         let height = canvas.height
 
         let player_image = document.getElementById('player-image')
-        let player = new Player((width / 2), height - player_image.height)
+        let player = new Player((width / 2), 320)
+        let snowLeft_image = document.getElementById('snowLeft-image')
+        let snowRight_image = document.getElementById('snowRight-image')
+        let snowMid_image = document.getElementById('snowMid-image')
+        let snow = new Snow(0, (player_image.height + 320), 12)
         let cloud_image = document.getElementById('cloud-image')
         let clouds = []
         for (let i = 0; i < 3; i++)
@@ -77,7 +94,19 @@
 
             ctx.drawImage(player_image, player.x - player_image.width / 2, player.y)
 
+            for (let i = 0; i < snow.size; i++) {
+                if (i == 0) {
+                    ctx.drawImage(snowLeft_image, snow.x, snow.y)
+                }
+                else if (i == snow.size - 1) {
+                    ctx.drawImage(snowRight_image, snow.x + (snowRight_image.width * i), snow.y)
+                }
+                else {
+                    ctx.drawImage(snowMid_image, snow.x + (snowRight_image.width * i), snow.y)
+                }
+            }
             player.tick()
+
             requestAnimationFrame(redraw)
         }
         requestAnimationFrame(redraw)
